95da5d00ac8466736f0d8e251685a67b33036ad9
zakaria <zakaria@zakaria-PC>
2023-05-15 13:00:51 +0430

    getting res3

--- LIP&Ctrl/Ctrl_Method1/MatlabCodes/test1.m
+++ LIP&Ctrl/Ctrl_Method1/MatlabCodes/test1.m
@@ -117,8 +117,8 @@ while Step(i) == 1
     s = s + 1;
     % Disturbance insertation
     if n+1 == 3 && t <= 0.1
-        Fy = 0; % max 85 
-        Fx = 340; % max 170
+        Fy = 50; % max 85(ankle) 
+        Fx = 0; % max 170(ankle)
     else
         Fy = 0;
         Fx = 0;
@@ -229,7 +229,7 @@ while Step(i) == 1
     % QP
     [qpresult, Opt_Vector] = controller_eng(t, T, Lnom, Wnom, L_min, L_max, W_min, W_max, T_min, T_max,...
           b_nom(n+1,1), b_nom(n+1,2), omega, zeta_mea_x, zeta_mea_y, r_vrp(n+2,1), r_vrp(n+2,2),...
-          [0 0], [0 0], 0, 0); %zeta_err_x, zeta_err_y, PcZMP_y(q,n+1), PcZMP_x(q,n+1)
+          zeta_err_x, zeta_err_y, PcZMP_y(q,n+1), PcZMP_x(q,n+1)); %zeta_err_x, zeta_err_y, PcZMP_y(q,n+1), PcZMP_x(q,n+1)
 
     T = (1/omega)*log(Opt_Vector(3));
     % update pattern parameter
